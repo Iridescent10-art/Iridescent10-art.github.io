77a924e16dda25dc10461ce9cc85eeed8b1e27f2
Iridescent10-art <guohao261024@gmail.com>
2025-08-07 22:51:42 +0800

    Site updated: 2025-08-07 22:51:41

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/08/07/这是另一篇新的博文/","2025/08/07/这是一篇新的博文/","2025/08/07/hello-world/"];function toRandomPost(){
+var posts=["2025/08/07/百业公告/","2025/08/07/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
